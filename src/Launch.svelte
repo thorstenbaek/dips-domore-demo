a48b6af707abae6812f7da77db0fb4766c31a749
Thor Stenbæk <tst@dips.no>
2022-05-13 09:57:20 +0200

    new PC

--- src/Launch.svelte
+++ src/Launch.svelte
@@ -8,6 +8,7 @@
             scope: 'launch openid fhirUser patient/*.read patient/*.write offline_access',
             redirectUri: './app',
             iss: 'https://vt-selecta-b.dips.local/DIPS-WebAPI/HL7/FHIR-R4',
+            //iss: 'https://vt-sb-srv.etl.dips.no/DIPS-WebAPI/HL7/FHIR-R4',
             launch:	'cdp2010025:encounter:resource'
 	    // iss=https://banan.dips.fhir.service.api.dips.no
 	    // launch=62247e85-c8c1-4047-90b3-e0b3a9c59600
